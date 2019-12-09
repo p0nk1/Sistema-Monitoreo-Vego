d9c01ee167cf13a1a60a9b773770449717cccbe4
p0nk1 <ponkialva@gmail.com>
2019-12-09 18:01:57 -0400

    Cambio nombre de BD y nuevo backup

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 	function conectar(){
-		$enlace = pg_connect("host='localhost' dbname=ffm port=5432 user=postgres password=aed09f75aa");
+		$enlace = pg_connect("host='localhost' dbname=siscovego port=5432 user=postgres password=aed09f75aa");
 		return $enlace;
 	}	
 ?>
